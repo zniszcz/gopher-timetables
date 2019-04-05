c74a377162816cb95c02298cc6c4ebb23cacf5f9
Adrian 'Zniszczo' Zniszczyński <adrian@zniszczynski.pl>
2019-04-05 16:27:36 +0200

    Initialise gopher clas

---
+++ index.js
@@ -0,0 +1,9 @@
+import moment from 'moment';
+
+class Gopher {
+    constructor({name, avalaibleFrom, avalaibleTo}) {
+        this.name = name;
+        this.avalaibleFrom = avalaibleFrom;
+        this.avalaibleTo = avalaibleTo;
+    }
+}
